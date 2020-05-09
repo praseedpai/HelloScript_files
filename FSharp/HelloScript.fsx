a1142ae1b0d4e6e663b9e8ebbad1d18ec947f58b
Vaisakh B <vaisakhb12@gmail.com>
2020-05-09 19:51:20 +0530

    Completed v1 of fsharp helloscript

--- FSharp/HelloScript.fsx
+++ FSharp/HelloScript.fsx
@@ -1,113 +1,357 @@
-// single line comments use a double slash
+"f#" // single line comments use a double slash; String expression
 (* multi line comments use (* . . . *) pair -end of multi line comment- *)
-
-// ======== "Variables" (but not really) ==========
-// The "let" keyword defines an (immutable) value
+(*
+    Variable and Binding
+    The "let" keyword defines an (immutable) value
+*)
 let myInt = 5
 let myFloat = 3.14
-let myString = "hello" //note that no types needed
+let myString = "hello"
 
-// ======== Lists ============
-let twoToFive = [2;3;4;5]        // Square brackets create a list with
-                                 // semicolon delimiters.
-let oneToFive = 1 :: twoToFive   // :: creates list with new 1st element
-// The result is [1;2;3;4;5]
-let zeroToFive = [0;1] @ twoToFive   // @ concats two lists
 
-// IMPORTANT: commas are never used as delimiters, only semicolons!
 
-// ======== Functions ========
-// The "let" keyword also defines a named function.
-let square x = x * x          // Note that no parens are used.
-square 3                      // Now run the function. Again, no parens.
+(* Conditional expression and logical operator *)
+if 3 < 4 then 117 else 118
 
-let add x y = x + y           // don't use add (x,y)! It means something
-                              // completely different.
-add 2 3                       // Now run the function.
 
-// to define a multiline function, just use indents. No semicolons needed.
+
+(*
+    Lists
+    Square brackets create a list with semicolon/new line delimiters
+*)
+let twoToFive = [ 2; 3; 4; 5 ]
+
+
+(* :: creates list with new 1st element *)
+let oneToFive = 1 :: twoToFive
+
+
+(* @ concats two lists *)
+let zeroToFive = [ 0; 1 ] @ twoToFive
+
+
+
+(* Pairs and tuples, product types *)
+let twoTuple = 1, 2
+let twoTuple2 = (1, 2)
+let threeTuple = ("a", 2, true)
+
+
+(* The empty tuple () of type unit, similar to void in C# and Java *)
+()
+
+
+
+(*
+    Record
+    Record types have named fields, Semicolons are separators
+*)
+type Person = { First: string; Last: string }
+let person1 = { First = "john"; Last = "Doe" }
+
+
+
+(*
+    Union
+    Union types have choices, vertical bars are separators
+*)
+type MeasurementUnit = Cm | Inch | Mile 
+
+type Name = 
+    | Nickname of string 
+    | FirstLast of string * string
+
+type Tree<'a> = 
+    | E 
+    | T of Tree<'a> * 'a * Tree<'a>
+
+let u = Inch
+let name = Nickname("John")
+let t = T(E,"John",E)	
+
+
+
+(* Enum *)
+type Gender = | Male = 1 | Female = 2
+
+let g = Gender.Male
+
+
+
+(* 
+    Class 
+    'new' keyword for secondary constructors
+    'member' keyword for defining members
+*)
+type Product (code:string, price:float) = 
+   let isFree = price=0.0 
+   
+   new (code) = Product(code,0.0)
+   
+   member this.Code = code 
+   member this.IsFree = isFree
+
+
+let p = Product("X123",9.99)
+let p2 = Product("X123")
+
+
+
+(*
+    Inteface
+    Same as class but all members are abstract
+    Abstract members have colon and type signature
+*)
+type IPrintable =
+   abstract member Print : unit -> unit
+
+
+
+(*
+    Struct
+    Uses "val" to define fields
+*)
+type Product= 
+   struct  
+      val code:string
+      val price:float
+      new(code) = { code = code; price = 0.0 }
+   end
+   
+let p = Product()
+let p2 = Product("X123")
+
+
+
+(*
+    Function
+    The "let" keyword also defines a named function
+    In F# returns are implicit -- no "return" needed.
+    A function always returns the value of the last expression used
+*)
+let square x = x * x
+square 3
+
+
+let add x y = x + y
+add 2 3
+
+
+(*
+    'Curried Functions' vs 'Function on product type'
+    int -> int -> int versus int * int -> int
+*)
+let addp (x, y) = x + y
+let addc x y = x + y
+
+let res1 = addp (17, 25)
+let res2 = addc 17 25
+
+let addSeventeen = addc 17
+let res3 = addSeventeen 25
+let res4 = addSeventeen 100
+
+
+(*
+    Multiline Function
+    Multiline function are defined using use indents; No semicolons needed
+*)
 let evens list =
-   let isEven x = x%2 = 0     // Define "isEven" as an inner ("nested") function
-   List.filter isEven list    // List.filter is a library function
-                              // with two parameters: a boolean function
-                              // and a list to work on
+    let isEven x = x % 2 = 0
+    List.filter isEven list
+evens oneToFive
 
-evens oneToFive               // Now run the function
 
-// You can use parens to clarify precedence. In this example,
-// do "map" first, with two args, then do "sum" on the result.
-// Without the parens, "List.map" would be passed as an arg to List.sum
-let sumOfSquaresTo100 =
-   List.sum ( List.map square [1..100] )
+(* Higher-order functions *)
+let rec map f xs =
+    match xs with
+    | [] -> []
+    | x :: xr -> f x :: map f xr
+let mul2 x = 2.0 * x
 
-// You can pipe the output of one operation to the next using "|>"
-// Here is the same sumOfSquares function written using pipes
+map mul2 [ 4.0; 5.0; 89.0 ]
+
+
+(* Recursive function declarations *)
+let rec fac n = if n = 0 then 1 else n * fac (n - 1)
+fac 7
+
+
+(* Mutually recursive function declarations *)
+let rec even n = if n = 0 then true else odd (n - 1)
+and odd n = if n = 0 then false else even (n - 1)
+
+even 10
+odd 10
+
+
+
+(* Type constraints *)
+let isLarge (x: float): bool = 10.0 < x
+isLarge 89.0
+
+
+(* Local let-binding *)
+let x = 42
+
+let r =
+    let x = 9 + 16
+    x * x
+
+r, x
+
+
+(*
+    You can use parens to clarify precedence.
+    In this example, do "map" first, with two args, then do "sum" on the result
+    Without the parens, "List.map" would be passed as an arg to List.sum
+*)
+let sumOfSquaresTo100 = List.sum (List.map square [ 1 .. 100 ])
+
+
+(*
+    Pipe
+    You can pipe the output of one operation to the next using "|>"
+*)
 let sumOfSquaresTo100piped =
-   [1..100] |> List.map square |> List.sum  // "square" was defined earlier
+    [ 1 .. 100 ] |> List.map square |> List.sum
 
-// you can define lambdas (anonymous functions) using the "fun" keyword
+
+(*
+    Anonymous functions
+    You can define anonymous functions using the "fun" keyword
+*)
 let sumOfSquaresTo100withFun =
-   [1..100] |> List.map (fun x->x*x) |> List.sum
+    [ 1 .. 100 ]
+    |> List.map (fun x -> x * x)
+    |> List.sum
 
-// In F# returns are implicit -- no "return" needed. A function always
-// returns the value of the last expression used.
 
-// ======== Pattern Matching ========
-// Match..with.. is a supercharged case/switch statement.
+
+(* Pattern Matching *)
+
 let simplePatternMatch =
-   let x = "a"
-   match x with
+    let x = "a"
+    match x with
     | "a" -> printfn "x is a"
     | "b" -> printfn "x is b"
-    | _ -> printfn "x is something else"   // underscore matches anything
+    | _ -> printfn "x is something else"
 
-// Some(..) and None are roughly analogous to Nullable wrappers
+
+(* Anonymous function defined by pattern matching *)
+
+let rec faca =
+    function
+    | 0 -> 1
+    | n -> n * faca (n - 1)
+faca 7
+
+
+
+(* Options *)
 let validValue = Some(99)
 let invalidValue = None
 
-// In this example, match..with matches the "Some" and the "None",
-// and also unpacks the value in the "Some" at the same time.
 let optionPatternMatch input =
-   match input with
+    match input with
     | Some i -> printfn "input is an int=%d" i
     | None -> printfn "input is missing"
 
 optionPatternMatch validValue
 optionPatternMatch invalidValue
 
-// ========= Complex Data Types =========
 
-// Tuple types are pairs, triples, etc. Tuples use commas.
-let twoTuple = 1,2
-let threeTuple = "a",2,true
 
-// Record types have named fields. Semicolons are separators.
-type Person = {First:string; Last:string}
-let person1 = {First="john"; Last="Doe"}
+(*
+    Modules
+    Modules are used to group functions together
+    Indentation is needed for each nested module.
+*)
+module ModuleExamples =
+    let add x y = x + y
+    let a = add 1 2
 
-// Union types have choices. Vertical bars are separators.
-type Temp = 
-  | DegreesC of float
-  | DegreesF of float
-let temp = DegreesF 98.6
+ModuleExamples.a
 
-// Types can be combined recursively in complex ways.
-// E.g. here is a union type that contains a list of the same type:
-type Employee = 
-  | Worker of Person
-  | Manager of Employee list
-let jdoe = {First="John";Last="Doe"}
-let worker = Worker jdoe
 
-// ========= Printing =========
-// The printf/printfn functions are similar to the
-// Console.Write/WriteLine functions in C#.
+
+(*
+    Printing
+    The printf/printfn functions are similar to the
+    Console.Write/WriteLine functions in C#
+*)
+
 printfn "Printing an int %i, a float %f, a bool %b" 1 2.0 true
-printfn "A string %s, and something generic %A" "hello" [1;2;3;4]
+printfn "A string %s, and something generic %A" "hello" [ 1; 2; 3; 4 ]
 
-// all complex types have pretty printing built in
-printfn "twoTuple=%A,\nPerson=%A,\nTemp=%A,\nEmployee=%A" 
-         twoTuple person1 temp worker
+(* All complex types have pretty printing built in *)
+printfn "twoTuple=%A,\nPerson=%A,\nTemp=%A,\nEmployee=%A" twoTuple person1 temp worker
 
-// There are also sprintf/sprintfn functions for formatting data
-// into a string, similar to String.Format.
+
+
+(* Exceptions *)
+
+exception IllegalHour
+
+let mins1 h =
+    if h < 0 || h > 23 then raise IllegalHour else h * 60
+
+try
+    (mins1 25)
+with IllegalHour -> -1
+
+
+
+(* Arrays *)
+let arr = [| 2; 5; 7 |]
+arr.[1]
+
+arr.[1] <- 11
+
+arr
+
+arr.Length
+
+
+
+(*
+    Assignment
+    '<-' Assigns a value to a variable
+*)
+let mutable mutableInt = 5
+mutableInt <- 7
+mutableInt
+
+
+
+(* References and updatable state *)
+
+let r = ref 177
+
+let v = !r
+
+r := 288
+
+!r
+
+
+
+(* Using methods from the .NET class library *)
+
+open System
+
+let z = Math.Sqrt 2.0
+
+
+
+(* .NET types and functions *)
+
+let tomorrow =
+    let nationalDebt = 14349539503882.02M
+    let perSecond = 45138.89M
+    nationalDebt + 86400M * perSecond
+
+
+let rec fac (n: bigint) = if n = 0I then 1I else n * fac (n - 1I)
+
+fac 104I
